4517f2ff5acbb0f3049650a38ce597bd5465343f
djhiggs <djhiggs@actx.edu>
2016-03-31 19:29:34 -0500

    Initial Upload
    This is the project initially uploaded to GitHub.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Here's a comment
+            //Here's another comment
+            //Yet another comment, but below this one there's code.
+            Console.WriteLine("Hello world.");
+            Console.ReadKey();
+        }
+    }
+}
